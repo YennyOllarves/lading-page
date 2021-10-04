2a3a6f257b42887ab11dca8c2cda2bbbc5b6a2c6
Yennys <yenny1989@gmail.com>
2021-10-03 20:49:53 -0500

    ending styles

---
+++ js/lightbox.js
@@ -0,0 +1,27 @@
+const imagenes = document.querySelectorAll('.img-gallery')
+const imagenLight = document.querySelector('.add-img');
+const contenedorLight = document.querySelector('.img-light')
+const closeLight = document.querySelector('.close')
+
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'));
+    })
+});
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+        hamburguer.style.opacity = '1';
+    }
+})
+
+
+const aparecerImagen = (imagen)=>{
+    imagenLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+    hamburguer.style.opacity = '0';
+}
